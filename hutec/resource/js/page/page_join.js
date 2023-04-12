49bb9f6497cb8e82433d5c70493662f8396d8455
lhk228 <lhk228@naver.com>
2023-04-12 11:18:39 +0900

    hutec update
    최신버전

--- hutec/resource/js/page/page_join.js
+++
@@ -1,8 +0,0 @@
-function checkPassword(){
-	if(pas1.value.length ==0 || pas2.value.length == 0 ) {result.innerText = ''}else{
-	result.innerText = pas1.value == pas2.value ? 'Matching' : 'Not Matching';
-	if(result.innerText == "Matching"){
-	document.getElementById("result").style.color = "var(--mint)"
-}else{
-	document.getElementById("result").style.color = "var(--red)"
-}}}
